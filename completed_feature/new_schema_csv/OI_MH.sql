3daa4498f8a670fb945254f0cfcaa5642761165a
Abir Khan <41482020+AbirKhan96@users.noreply.github.com>
2020-04-08 16:26:57 +0530

    Add files via upload

---
+++ completed_feature/new_schema_csv/OI_MH.sql
@@ -0,0 +1,30 @@
+﻿with wsul as (
+select 
+	month_n, state_n,
+	sum(no_chld_svr_wasting) as svrly_wstd,
+	sum(no_mdrtly_wasted_chld) as mdrtly_wstd,
+	sum(no_svrly_stunted_chld) as svrly_stntd,
+	sum(no_mdrtly_stunted_chld) as mdrtly_stntd,
+	sum(no_svrly_uw_chld) as svrly_uw,
+	sum(no_mdrtly_uw_chld) as mdrtly_uw,
+	sum(no_nwborns_wid_lbw) as nb_lbw,
+	sum(total_no_chld_ht_msrd) as total_no_chld_ht_msrd,
+	sum(total_no_chld_ht_wt_msrd) as total_no_chld_ht_wt_msrd,
+	sum(total_no_chld_wgd) as total_no_chld_wgd,
+	sum(total_no_chld_born_wgd_in_mnth) as total_no_chld_born_wgd_in_mnth
+        from  consolidated_child_ndj
+group by 
+consolidated_child_ndj.month_n, consolidated_child_ndj.state_n
+order by 
+consolidated_child_ndj.month_n,
+consolidated_child_ndj.state_n)
+
+select month_n, state_n, mdrtly_stntd,svrly_stntd, svrly_wstd, mdrtly_wstd, svrly_uw, mdrtly_uw,nb_lbw,
+round(svrly_wstd + mdrtly_wstd) as wstd_chld,
+round(svrly_stntd + mdrtly_stntd) as stntd_chld,
+round(svrly_uw + mdrtly_uw) as uw_chld,
+round(((svrly_stntd + mdrtly_stntd)/NULLIF(total_no_chld_ht_msrd,0))*100,2) as stunting_percent,
+round(((svrly_wstd + mdrtly_wstd)/NULLIF(total_no_chld_ht_wt_msrd,0))*100,2) as wasting_percent,
+round(((svrly_uw + mdrtly_uw)/NULLIF(total_no_chld_wgd,0))*100,2) as underweight_percent,
+round((nb_lbw/NULLIF(total_no_chld_born_wgd_in_mnth,0))*100,2) as low_birth_weight_percent
+from wsul;
---
+++ completed_feature/new_schema_csv/PI_MH.sql
@@ -0,0 +1,26 @@
+﻿with wsul as (
+select 
+	month_n, state_n,
+	sum(no_anemic_wmn) as no_anemic_wmn,
+	sum(no_pregnant_women) as no_pregnant_women,
+	sum(no_tenatus_cmpltd) as no_tenatus_cmpltd,
+	sum(no_wmn_rstg_drg_prgncy) as no_wmn_rstg_drg_prgncy,
+	sum(no_wmn_etg_xtr_ml_drg_prgncy) as no_wmn_etg_xtr_ml_drg_prgncy,
+	sum(no_trmstr_3wmn_cnsld_imdtbf) as no_trmstr_3wmn_cnsld_imdtbf,
+	sum(no_wmn_1_anc_vst_dlvry) as no_wmn_1_anc_vst_dlvry,
+	
+	sum(no_wmn_4_anc_vst_dlvry) as no_wmn_4_anc_vst_dlvry
+        from pregnant_women_ndj
+group by 
+pregnant_women_ndj.month_n, pregnant_women_ndj.state_n
+order by 
+pregnant_women_ndj.month_n,
+pregnant_women_ndj.state_n)
+
+select month_n, state_n, no_anemic_wmn,no_pregnant_women, no_tenatus_cmpltd, no_wmn_rstg_drg_prgncy, no_wmn_etg_xtr_ml_drg_prgncy,no_trmstr_3wmn_cnsld_imdtbf, no_wmn_4_anc_vst_dlvry,
+round((no_anemic_wmn/no_pregnant_women)*100 , 2) as prnt_anwmic_wmn,
+round((no_wmn_4_anc_vst_dlvry/no_pregnant_women)*100 , 2) as prnt_4_anc_vst_dlvry,
+round((no_wmn_etg_xtr_ml_drg_prgncy/no_pregnant_women)*100 , 2) as prnt_wmn_etg_xtr_ml_drng_prgncy,
+round((no_wmn_rstg_drg_prgncy/no_pregnant_women)*100 , 2) as prnt_wmn_rstg_drg_prgncy,
+round((no_trmstr_3wmn_cnsld_imdtbf/no_pregnant_women)*100 , 2) as prnt_trmstr_3wmn_cnsld_imdtbf
+from wsul;
